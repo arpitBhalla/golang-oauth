cf22803ed5380538214382a0086795756bbaf440
Arpit Bhalla <arpitbhalla2001@gmail.com>
2021-04-21 15:51:45 +0530

    Improved

--- app.go
+++ app.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"gawds/src/routes"
@@ -12,5 +13,16 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/user", routes.CreateUser).Methods("POST")
 	r.HandleFunc("/user/{key}", routes.GetUser).Methods("GET")
-	http.Handle("/", r)
+
+	r.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		rw.WriteHeader(http.StatusOK)
+		rw.Write([]byte("Server Running"))
+	})
+
+	log.Println("Server Stated")
+
+	err := http.ListenAndServe(":8080", r)
+	if err != nil {
+		log.Fatalln("Error Occured", err)
+	}
 }
